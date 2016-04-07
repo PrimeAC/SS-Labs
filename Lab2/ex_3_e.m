f5ffebc3a0bbcea9e5823edb2aa0a8d87eaf8b26
DReigada <daniel.reigada@gmail.com>
2016-04-07 13:32:29 +0100

    Add exercise 3 e)

---
+++ Lab2/ex_3_e.m
@@ -0,0 +1,27 @@
+addpath('resources');
+n = 0:50;
+r=(-1).^n.*ones(1,length(n));
+
+y1 = equacao_diferencas(r, -1.1, -0.1, 0);
+y2 = equacao_diferencas(r, -1.1, 0.1, 0);
+y3 = equacao_diferencas(r, -1.1, 0.2, 0);
+y4 = equacao_diferencas(r, -1.1, 2, 0);
+y5 = equacao_diferencas(r, -1.1, 2.1, 0);
+y6 = equacao_diferencas(r, -1.1, 2.2, 0);
+
+
+subplot(3,2,1);
+stem(n,y1); xlabel('n'); ylabel('y(n)'); title('K=-0.1');
+subplot(3,2,2);
+stem(n,y2); xlabel('n'); ylabel('y(n)'); title('K=0.1');
+subplot(3,2,3);
+stem(n,y3); xlabel('n'); ylabel('y(n)'); title('K=0.2');
+subplot(3,2,4);
+stem(n,y4); xlabel('n'); ylabel('y(n)'); title('K=2');
+subplot(3,2,5);
+stem(n,y5); xlabel('n'); ylabel('y(n)'); title('K=2.1');
+subplot(3,2,6);
+stem(n,y6); xlabel('n'); ylabel('y(n)'); title('K=2.2');
+
+
+rmpath('resources');
